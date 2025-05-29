93eb1d07539a690f3f9b83be8c8931fec41ee881
init.defaultBranch <benlimpic@gmail.com>
2025-05-28 22:13:14 -0700

    Fix CSP header and bind to Heroku port

--- src/main/java/com/authentication/demo/Security/CSPFilter.java
+++ src/main/java/com/authentication/demo/Security/CSPFilter.java
@@ -3,6 +3,7 @@ package com.authentication.demo.Security;
 
 import java.io.IOException;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.Filter;
@@ -12,6 +13,7 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Profile("demo")
 @Component
 public class CSPFilter implements Filter {
 
@@ -22,7 +24,8 @@ public class CSPFilter implements Filter {
         HttpServletResponse res = (HttpServletResponse) response;
 
         // Allow iframe embedding only from self and subdomains of benlimpic.info
-        res.setHeader("Content-Security-Policy", "frame-ancestors 'self' *.benlimpic.info");
+        res.setHeader("Content-Security-Policy", "frame-ancestors 'self' https://portfolio.benlimpic.info https://shelved-demo-app.benlimpic.info");
+
 
         chain.doFilter(request, response);
     }
